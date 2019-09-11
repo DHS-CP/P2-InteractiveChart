93688e3d9496438724c17a6f5c1ba35d4b923716
Gaurav Agarwal <gaurava@Gauravs-MacBook-Air.local>
2019-09-11 00:14:39 -0700

    Updated and added new javadoc comments

--- Assign1SeatingChart-master/Classroom.java
+++ Assign1SeatingChart-master/Classroom.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import greenfoot.*;
 import java.util.ArrayList;
+ 
 /**
  * Classroom prepares the world and sets up the background
  * 
@@ -10,18 +11,22 @@ import java.util.ArrayList;
 public class Classroom extends World
 {
     private ArrayList<Object> listo = new ArrayList<Object>();
+    
     /**
      * Classroom creates the new world with cells and prepares it
-    */
+     */
     public Classroom()
     {    
         super(10, 6, 130); 
+
         prepare();
     }
+    
     /**
-     * Prepare prepares the world for the start of the program and creates the initial objects and adds them to the world
-    */
-     private void prepare()
+     * Prepare prepares the world for the start of the program and creates 
+     * the initial objects and adds them to the world
+     */
+    private void prepare()
     {
         GauravAgarwal gauravagarwal = new GauravAgarwal("Gaurav", "Agarwal", 1, 1);
         addObject(gauravagarwal, 1, 1);
--- Assign1SeatingChart-master/Teacher.java
+++ Assign1SeatingChart-master/Teacher.java
@@ -4,7 +4,7 @@ import greenfoot.*;
  * Teacher declares the classes to play the sound and sit GauravAgarwal down 
  * 
  * @author Gaurav Agarwal
- * @version Sep 10, 2019
+ * @version Sep 8, 2019
  */
 public abstract class Teacher extends Actor
 {
@@ -16,13 +16,16 @@ public abstract class Teacher extends Actor
     * sayName plays the sound file that says the name
     * @param String myNameFile (string of sound file)
     */
-   public void sayName(String myNameFile){
+   public void sayName(String myNameFile) {
         Greenfoot.playSound(myNameFile);
    }
+   
    /**
     * sitDown sets the variable to sit down at the correct spot
-   */
-   public void sitDown(){
+    */
+   public void sitDown() {
         setLocation(mySeat,myRow);
    }
+        
+ 
 }
