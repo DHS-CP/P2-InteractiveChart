e197d0a500c6d838c9dd3893ee4cab7f6420675b
Gaurav Agarwal <gaurava@Gauravs-MacBook-Air.local>
2019-09-10 23:28:33 -0700

    updated the javadoc comments for the cod

--- Assign1SeatingChart-master/CSALearnedSoFar.java
+++ Assign1SeatingChart-master/CSALearnedSoFar.java
@@ -1,12 +1,9 @@
 /**
- * Write a description of interface CSALearnedSoFar here.
+ * CSALearnedSoFar allows Greenfoot to output a string that is about what was learned in AP CSA
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Gaurav Agarwal
+ * @version Sep 10, 2019
  */
-public interface CSALearnedSoFar  
-{
-    // method signatures - implement the signature below in your own class. Make sure to
-    //                     match the parameter list and return type
-    public void LearnedSoFar();
+public interface CSALearnedSoFar {
+    public void learnedSoFar(String LearnedSoFarSentence);
 }
--- Assign1SeatingChart-master/StudentLeadership.java
+++ Assign1SeatingChart-master/StudentLeadership.java
@@ -1,12 +1,9 @@
 /**
- * Write a description of interface StudentLeadership here.
+ * StudentLeadership allows Greenfoot to output a string about my leadership
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Gaurav Agarwal
+ * @version Sep 10, 2019
  */
-public interface StudentLeadership  
-{
-    // method signatures - implement the signature below in your own class. Make sure to
-    //                     match the parameter list and return type
-    public void myRoleInLeadership();
+public interface StudentLeadership {
+    public void myRoleInLeadership(String myLeadership);
 }
