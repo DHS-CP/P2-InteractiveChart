bae1a955554399c59ed66393af19d971b1723501
Gaurav Agarwal <gaurava@Gauravs-MacBook-Air.local>
2019-09-10 23:23:23 -0700

    updated javadoc comments for code

---
+++ Assign1SeatingChart-master/myHobbyInterest.java
@@ -0,0 +1,9 @@
+/**
+ * myHobbyInterest allows Greenfoot to output a string that is about my hobbies and interests
+ * 
+ * @author Gaurav Agarwal
+ * @version Sep 10, 2019
+ */
+public interface myHobbyInterest {
+    public void myHobby(String myHobbySentence);
+}
