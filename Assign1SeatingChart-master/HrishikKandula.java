0113f417057785dea05154b84bc30c283438d49e
unknown <kandula0118@mydusd.org>
2019-09-11 09:47:07 -0700

    Added javadoc comments

--- Assign1SeatingChart-master/HrishikKandula.java
+++ Assign1SeatingChart-master/HrishikKandula.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.Random; 
 /**
- * The HrishikKandula class can be used as a model for your own class that represents you and your seating location in AP CSA
+ * The HrishikKandula class is the model for the actor that represents the image and seating location 
+ * of Hrishik Kandula in AP CSA
  * 
- * @author Mr. Kaehms
- * @version 2.0 Aug 13, 2019
+ * @author Hrishik Kandula
+ * @version 2.1 Sep 10, 2019
  */
 public class HrishikKandula extends Student implements SpecialInterestOrHobby, NumberOfSiblings
 {
     /**
      * Constructor for the HrishikKandula class.
-     * Constructors are special methods with the same exact name as the class name.  
-     * Constructors to not have return types.
-     * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
-     *  lists to initalize for different conditions (depending on what constructors have been written.
+     * Increments the numStudents static variable in Student class to keep track of the number of instances created.
+     * Sets up the image and sound files used in this actor.
+     * 
      * @param String f (firstname)
      * @param String l (lastname)
      * @param int r (row of seating arrangement)
@@ -21,6 +21,7 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
      * 
      */
     public HrishikKandula(String f, String l, int r, int s) {
+        numStudents++; // increments the number of students protected static variable in Student class
         firstName=f;
         lastName=l;
         myRow=r;
@@ -32,11 +33,12 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
         sitting=true;
     }
     /**
-     * Default constructor, if you don't pass in a name and seating location
-     * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
-     * of the classroom.
+     * Default constructor for HrishikKandula actor.
+     * Sets up first & last name and seating position variables.
+     * Initializes the image and sound file variables and sets up the sitting image.
      */
     public HrishikKandula() {
+        numStudents++; // increments the number of students protected static variable in Student class
         firstName="Hrishik";
         lastName="Kandula";
         myRow=2;
@@ -50,8 +52,9 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
     }
 
      /**
-     * Act - do whatever the HrishikKandula actor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * act method sets up code for handling mouse click.
+     * When the actor is in sitting position and mouse is clicked, it stands up, says the name and
+     * does animations. If it is in standing position, it will answer questions.
      */   
     public void act() 
     {
@@ -90,12 +93,15 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
      * some mechanism that allows the student to sit down once the Q&A session ends.  You can use this basic model, or come up
      * with some additional class and object that represents a blackboard, or a talking cartoon bubble etc. If you provide extra
      * classes, make sure to fully document so other students can use the same interface.
+     * This method allows the user to ask a question and will answer the question.
+     * If the user types yes to "May I sit down?" question, it will sit down.
+     * Sample questions to ask are "Which subject is hard for you in CSA?", "How many siblings do you have?".
      */
     public void answerQuestion(){
-        String q=Greenfoot.ask("What would you like to know");
+        String q=Greenfoot.ask("What would you like to know?");
         if (q.contains("hard")){
             q=Greenfoot.ask("Merge Sort, 2D arrays, Interfaces, Conditionals, and constructors were difficult... May I sit down?");
-
+    
         }
         else if(q.contains("brother")){
             q=Greenfoot.ask("I have " + numberOfBrothers() +" brothers... May I sit down?");
@@ -107,9 +113,9 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
             q=Greenfoot.ask("I have " + numberOfSiblings() +" siblings... May I sit down?");
         }
         else{
-          q=Greenfoot.ask("I don't understand the question... May I sit down?"); 
+            q=Greenfoot.ask("I don't understand the question... May I sit down?"); 
         }
-         if (q.equals("yes")){
+        if (q.equals("yes")){
             Greenfoot.delay(10);
             sitDown();
         }
@@ -117,12 +123,18 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
     /**
      * This is a local method specific to the HrishikKandula class used to animate the character once the image is clicked on.
      * You can write your own methods to perform your own animation for your character/avatar.
+     * This method creates HrihikKandula actors and fills up 6x10 grid with them.
+     * After each row of the grid is filled, backgroud color is set to a random color.
+     * Once the grid is filled, the actors are removed.
+     * Then HrishikKandula avatar walks to the right and comes back to original position.
      */
     public void coolMethod(){
         Random r = new Random();
         GreenfootImage bg = getWorld().getBackground();
         GreenfootImage bgBak = new GreenfootImage(bg);
         GreenfootImage screen = new GreenfootImage(bg.getWidth(), bg.getHeight());
+        // create a 2 dimensional array to keep the actors so that they can be removed
+        // after the animation is complete.
         HrishikKandula[][] hrishikActorsArray = new HrishikKandula[6][10];
         for (int i = 0; i < 6; i++)
         {
@@ -133,10 +145,12 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
                 Greenfoot.delay(5);
                 hrishikActorsArray[i][j] = hrishikkandula;
             }
+            // set a random color and fill the screen
             screen.setColor(new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
             screen.fill();
             bg.drawImage(screen, 0,0);
         }
+        // remove all the actors added
         for (int i = 0; i < 6; i++)
         {
             for (int j = 0; j < 10; j++)
@@ -145,6 +159,7 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
                 Greenfoot.delay(2);
             }
         }
+        // set the background to original image
         bg.drawImage(bgBak, 0,0);
         // move right
          for (int i=1;i<=8;i++){
@@ -157,15 +172,27 @@ public class HrishikKandula extends Student implements SpecialInterestOrHobby, N
             Greenfoot.delay(10);
         }
     }
+    /**
+     * Prints my hobby to screen.
+     */
     public void myHobby(String s) {
         System.out.println(s);
     }
+    /**
+     * Returns the number fo siblings for HrishikKandula
+     */
     public int numberOfSiblings(){
         return 1;
     }
+    /**
+     * Returns the number of brothers for HrishikKandula
+     */
     public int numberOfBrothers(){
         return 0;
     }
+    /**
+     * Returns the number of sisters for HrishikKandula
+     */
     public int numberOfSisters(){
         return 1;
     }
